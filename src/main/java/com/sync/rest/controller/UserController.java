7102927b0f61d06f4094a0e71cd1c4bad0513848
TheJavaGuy <40058469+TejalWaingankar@users.noreply.github.com>
2019-05-09 17:29:33 +0530

    Spring Boot Rest initial commit.
    Spring Boot Rest initial commit.

---
+++ src/main/java/com/sync/rest/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.sync.rest.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.sync.rest.exception.UserNotFoundException;
+import com.sync.rest.model.User;
+import com.sync.rest.service.UserDaoService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDaoService service;
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+
+		if (user == null)
+			throw new UserNotFoundException("id-" + id);
+
+		return user;
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+
+		if (user == null)
+			throw new UserNotFoundException("id-" + id);
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+}
