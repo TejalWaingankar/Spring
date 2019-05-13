6c79b5f58700a721773718dbfcef1d4bdfc7ff26
TejalWaingankar <tejal.waingankar@gmail.com>
2019-05-13 15:26:17 +0530

    Added response entity as return.

--- src/main/java/com/sync/rest/controller/UserController.java
+++ src/main/java/com/sync/rest/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.sync.rest.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sync.rest.exception.UserNotFoundException;
 import com.sync.rest.model.User;
@@ -23,7 +21,7 @@ public class UserController {
 
 	@Autowired
 	private UserDaoService service;
-	
+
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
@@ -40,21 +38,20 @@ public class UserController {
 	}
 
 	@DeleteMapping("/users/{id}")
-	public void deleteUser(@PathVariable int id) {
+	public ResponseEntity<User> deleteUser(@PathVariable int id) {
 		User user = service.deleteById(id);
 
 		if (user == null)
 			throw new UserNotFoundException("id-" + id);
+
+		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
 
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
 
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
-				.toUri();
-
-		return ResponseEntity.created(location).build();
+		return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
 
 	}
 }
