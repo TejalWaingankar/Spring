d5db398f8513e564dd847cb2be4b3852be04917f
Tejal Waingankar <tejal.waingankar@gmail.com>
2025-03-09 20:05:12 +0530

    Added Junit, updated test cases.

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,15 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import com.sync.rest.RestfulWebServicesApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = RestfulWebServicesApplication.class)
 public class RestfulWebServicesApplicationTests {
 
 	@Test
+    //@Disabled
 	public void contextLoads() {
 	}
 
